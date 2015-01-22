54970a52a0ec53fde9d554bb5af26e3c99ea38b6
lyh <lyhopq@gmail.com>
2015-01-22 22:27:23 +0800

    add test

--- test/conway_test.cpp
+++ test/conway_test.cpp
@@ -1,20 +1,54 @@
 #include <gtest/gtest.h>
+#include <iostream>
+#include <fstream>
 #include "cell_factory.h"
 #include "alive.h"
 #include "dead.h"
+#include "conway.h"
+
+using namespace std;
 
 TEST(cell_factory_test, gernate_new_cell)
 {
 	CellFactory factory;
-    EXPECT_EQ('$', factory.NewCell(0, 0, '1')->show());
-    EXPECT_EQ(' ', factory.NewCell(0, 0, '0')->show());
+    EXPECT_EQ('$', factory.NewCell(0, 0, '1')->encode());
+    EXPECT_EQ(' ', factory.NewCell(0, 0, '0')->encode());
 }
 
-TEST(conway_game_test, one_step)
+
+TEST(cell_test, check_cell_status_change)
+{
+	Alive alive(0, 0);
+	Dead dead(0, 1);
+
+    EXPECT_FALSE(alive.isStatusChanged(2));
+    EXPECT_FALSE(alive.isStatusChanged(3));
+    EXPECT_TRUE(alive.isStatusChanged(1));
+    EXPECT_TRUE(alive.isStatusChanged(4));
+
+
+    EXPECT_FALSE(dead.isStatusChanged(2));
+    EXPECT_FALSE(dead.isStatusChanged(4));
+    EXPECT_TRUE(dead.isStatusChanged(3));
+}
+
+TEST(conway_test, check_propagate_result)
 {
 	Conway game;
 	game.getSeed();
-	game.step();
+	game.propagate();
+	game.dump();
+
+	ifstream ifs("final.txt");
+	string line;
+
+	getline(ifs, line);
+	EXPECT_EQ(" $ ", line);
+	getline(ifs, line);
+	EXPECT_EQ("$ $", line);
+	getline(ifs, line);
+	EXPECT_EQ(" $ ", line);
+
 }
 
 
